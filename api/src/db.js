5b3a495f7df9e6d545122adc4e557896412f059d
Jose Manuel Puello Cortina <puelloj66@gmail.com>
2022-04-12 17:34:30 -0500

    relaciones db

--- api/src/db.js
+++ api/src/db.js
@@ -31,10 +31,15 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 
+const { Ticket, Pelicula, Categoria, Sucursal, User } = sequelize.models; 
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
+Pelicula.hasMany(Ticket); 
+Sucursal.hasMany(Ticket); 
+User.hasMany(Ticket); 
+Pelicula.belongsTo(Categoria);
 
 
 
