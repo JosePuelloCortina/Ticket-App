f37c0772d942ac8d80bd1fe3d68b1c86eb09cfae
Jose Manuel Puello Cortina <puelloj66@gmail.com>
2022-04-15 17:22:34 -0500

    rutas sucursales

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,11 +5,13 @@ const router = Router();
 const user = require("./User")
 const movies = require("./movie");
 const admin = require("./Admin");
+const sucursales = require("./sucursales");
 
 
 router.use("/user", user);
 router.use("/movies", movies);
 router.use("/admin", admin);
+router.use("/sucursales", sucursales);
 
-
+ 
 module.exports = router;
