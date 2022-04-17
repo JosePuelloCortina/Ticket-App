f8d51e9785e86b261e8b179f9f354a380ea166b5
David-j787 <davidpatejo@gmail.com>
2022-04-17 15:55:46 -0300

    a

--- client/src/Componentes/Login/LoginUser.jsx
+++ client/src/Componentes/Login/LoginUser.jsx
@@ -1,57 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState} from 'react'
+import { useDispatch } from 'react-redux';
+import {login } from '../../redux/actions/index';
 import { Link, useNavigate } from "react-router-dom";
-import GoogleLogin from "react-google-login";
+//import GoogleLogin from "react-google-login";
 import uno from '../Image/uno.jpg'
 import dos from '../Image/dos.jpg'
 import tres from '../Image/tres.jpg';
 
 
-// const initialLogin = {
-//     contrasena: '',
-//     email: ''
-//   }
-
-export default function Login() {
-  //const [formlogin, setFormLogin] = useState()
-  /// const [error, setError] = useState()
-  // const navigate = useNavigate()
-
-  //const dispatch= useDispatch()
-
-  const handleChange = (e) => {
-    //   setFormLogin({
-    //     ...formlogin,
-    //     [e.target.name]: e.target.value
-    //   })
-    //   const errors = {
-    //     ...error,
-    //     [e.target.name]: ''
-    //   }
-    //   setError(errors)
-    //   console.log(e.target.value)
-  };
-  const handleSubmit = (e) => {
-    //e.preventDefault()
-    //const errors = {
-    //     ...error,
-    // contrasena: '',
-    // email: '',
-    // }
-    // setError(errors)
-    //dispatch (login(formlogin))
-    //console.log(formlogin)
-    // navigate()
-  };
-
-  // useEffect(()=>{
-  //   dispatch()
-  // }, [])
-
-  const responseGoogle = (response) => {
-    console.log(response);
-  };
-
+const userInfo = {
+    password:'',
+    email: ''
+  }
+  
+  export default function Login() {
+  
+    const [formlogin, setFormLogin] = useState(userInfo)
+    const [error, setError] = useState()
+    const navigate = useNavigate()
+  
+    const dispatch= useDispatch()
+  
+    const handleChange = (e) => {
+  
+      setFormLogin({
+        ...formlogin,
+        [e.target.name]: e.target.value
+      })
+      const errors = {
+        ...error,
+        [e.target.name]: ''
+      }
+      setError(errors)
+  
+      console.log(e.target.value)
+     }
+    const handleSubmit = (e) => {
+      e.preventDefault()
+       const errors = {
+        ...error,
+        password:'',
+        email: '',
+     }
+     setError(errors)
+  
+  
+      dispatch (login(formlogin))
+      console.log(formlogin)
+      navigate('/home')
+    }
+  
+    // useEffect(()=>{
+    //   dispatch()
+    // }, [])
+  // const responseGoogle =(response)=>{
+  //   console.log(response)
+  // }
+    
   return (
       <div className='row conteiner p-4' >
         <div className='col-md-8'>
@@ -94,44 +99,34 @@ export default function Login() {
             <span className="visually-hidden">Next</span>
           </button>
         </div>
-      </div>
-
-      {/* FORM LOGIN */}
-      <div className="col-md-4">
-        <div className="mt-5 ms-5">
-          <h1 className="text-center">Login</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              {" "}
-              {/* CORREO */}
-              <label htmlFor="exampleInputEmail1">Correo</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Ingresar Correo"
-                name="email"
-                onChange={handleChange}
-                value={""}
-              />
-              {/* Contraseña  */}
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputPassword1">Contraseña</label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"
-                name="contrasena"
-                onChange={handleChange}
-                value={""}
-              />
-              <small>
-                El equipo de Ticket - App bajo ninguna circunstancia pedira su
-                correo o contraseña.{" "}
-              </small>
-            </div>
-            {/* <div className="form-group form-check">
+  
+        {/* FORM LOGIN */}
+        <div className='col-md-4'>
+          <div className='mt-5 ms-5'>
+            <h1 className='text-center'>Login</h1>
+            <form onSubmit={handleSubmit} >
+              <div className="form-group"> {/* CORREO */}
+                <label htmlFor="exampleInputEmail1">Correo</label>
+                <input type="email" 
+                className="form-control" 
+                placeholder="Ingresar Correo" 
+                name='email' 
+                onChange={handleChange} 
+                value={formlogin.email} />
+                {/* Contraseña  */}
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Contraseña</label>
+                <input type="password" 
+                className="form-control" 
+                id="exampleInputPassword1" 
+                placeholder="Password" 
+                name='password'
+                onChange={handleChange} 
+                value={formlogin.password}/>
+                <small >El equipo de Ticket - App bajo ninguna circunstancia pedira su correo o contraseña. </small>
+              </div>
+              {/* <div className="form-group form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                 <label className="form-check-label" htmlFor="exampleCheck1">Comprendo</label>
               </div> */}
@@ -153,26 +148,15 @@ export default function Login() {
               </div>
   
             </form>
-            <GoogleLogin
+            {/* <GoogleLogin
             clientId="533216406102-cnhnnd2b69dvbkt69reehsd2e7stn4t4.apps.googleusercontent.com"
             buttonText="Login with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
             cookiePolicy={"single_host_origin"}
-          />
+          /> */}
   
-            {/* <div className='text-center '>
-  
-              <Link to='/resetPassword' style={{
-                color: '#0066ff',
-                textDecoration: 'none',
-                margin: '5px',
-                fontWeight: 'bold'
-  
-              }} >
-                ¿ Olvidaste tu contraseña ?
-              </Link>
-              </div>               */}
+           
         </div>
       </div>
     </div>
