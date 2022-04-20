bbb845d2bd7fb54e6d5465caa3ea2831e5913a4e
Ivan Torrez <itorrez1506@gmail.com>
2022-04-20 17:34:17 -0400

    Formularios con estilos

--- client/src/Componentes/Login/LoginUser.jsx
+++ client/src/Componentes/Login/LoginUser.jsx
@@ -1,40 +1,108 @@
-import React, { useEffect, useState} from 'react'
+import React, { useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../../redux/actions/index';
 import { Link, useNavigate } from "react-router-dom";
 import GoogleLogin from "react-google-login";
 import { postUser } from '../../redux/actions/index';
-import uno from '../Image/uno.jpg'
-import dos from '../Image/dos.jpg'
-import tres from '../Image/tres.jpg';
+import { Button, Checkbox, CssBaseline, FormControlLabel, FormGroup, TextField, Typography } from '@material-ui/core';
+import Transition from '../image_transition/Transition';
 
-
+export function validate(input) {
+  let error = {};
+  const regexp_passwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+	const regexp_correo = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  if (!regexp_correo.test(input.email))
+    error.email = 'Correo no válido';
   
+  if(!regexp_passwd.test(input.password))
+    error.password = "Contraseña no válida";
+  
+  return error;
+};
+
+const useStyles = makeStyles((theme)=>({
+  logInContainer: {
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    position: 'relative',
+    minHeight: '100vh',
+    },
+  right:{
+    backgroundColor: '#fff',
+    width: '50%',
+    maxWidth: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  // aqui el frame div derecho
+  forgot:{
+    marginTop: '15px',
+    width: '338px',
+    height: '24px',
+    left: '780px',
+    top: '634px',
+    fontFamily: `"Avenir LTS Regular", sans-serif`,
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '14px',
+    lineHeight: '19px',
+    display: 'flex',
+    alignItems: 'center',
+    color: '#000000',
+    boxSizing: 'border-box',
+    borderColor: '1px solid red',
+  },
+  divSingUp:{
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    maxWidth:'100%',
+    height:'fitContent',
+    overflow:'hidden auto',
+    padding: '1rem',
+  },
+  form:{
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    gap:'12px',
+  },
+}));
+
   export default function Login() {
-  
+    
     const user = useSelector(state => state.userInfo);
-
     const [input, setInput] = useState({
-      password:'',
-      email: ''
+      email: "",
+      password:"",
     })
+    const [check, setCheck] = useState(false);
     const [error, setError] = useState()
     const navigate = useNavigate()
   
     const dispatch= useDispatch()
   
     const handleChange = (e) => {
-  
       setInput({
         ...input,
+        [e.target.name]: e.target.value,
+      });
+  
+      setError(validate({
+        ...input,
         [e.target.name]: e.target.value
-      })
-      const errors = {
-        ...error,
-        [e.target.name]: ''
-      }
-      setError(errors)
-     }
+      }));
+     };
+    
+    const handleChangeCheck = (e) =>{
+      setCheck(!check);
+    }
+
     const handleSubmit = (e) => {
       e.preventDefault()
        const errors = {
@@ -49,9 +117,6 @@ import tres from '../Image/tres.jpg';
       navigate('/home')
     }
   
-    // useEffect(()=>{
-    //   dispatch()
-    // }, [])
   const onGoogleSucces = (response)=>{
     console.log(response);
     const usuario = response.profileObj;
@@ -71,106 +136,83 @@ import tres from '../Image/tres.jpg';
   const onFailGoogle = () => {
     alert("Algo salió mal")
   }
+
+  const classes = useStyles();
   return (
-      <div className='row conteiner p-4' >
-        <div className='col-md-8'>
-  
-          <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img className="tamaño" src={uno} alt="First slide" />
-              </div>
-              <div className="carousel-item">
-                <img className="tamaño" src={dos} alt="Second slide" />
-              </div>
-              <div className="carousel-item">
-                <img className="tamaño" src={tres} alt="Third slide" />
-              </div>
-            </div>
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleControls"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleControls"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-  
-        {/* FORM LOGIN */}
-        <div className='col-md-4'>
-          <div className='mt-5 ms-5'>
-            <h1 className='text-center'>Login</h1>
-            <form onSubmit={handleSubmit} >
-              <div className="form-group"> {/* CORREO */}
-                <label htmlFor="exampleInputEmail1">Correo</label>
-                <input type="email" 
-                className="form-control" 
-                placeholder="Ingresar Correo" 
-                name='email' 
-                onChange={handleChange} 
-                value={input.email} />
-                {/* Contraseña  */}
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Contraseña</label>
-                <input type="password" 
-                className="form-control" 
-                id="exampleInputPassword1" 
-                placeholder="Password" 
-                name='password'
-                onChange={handleChange} 
-                value={input.password}/>
-                <small >El equipo de Ticket - App bajo ninguna circunstancia pedira su correo o contraseña. </small>
-              </div>
-              <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">Comprendo</label>
-              </div>
-              
-                <button type="submit" className="btn btn-primary">Entrar</button>
-              
-  
-              <div className='text-center '>
-                <span>¿No tienes cuenta?</span>
-                <Link to='/register' style={{
-                  color: '#0066ff',
-                  textDecoration: 'none',
-                  margin: '5px',
-                  fontWeight: 'bold'
-  
-                }} >
-                  Registrarse
-                </Link>
-              </div>
-  
-            </form>
+    <div className={classes.logInContainer}>
+      <CssBaseline/>
+      <Transition/>
+      <div className={classes.right}>
+        <div className={classes.divSingUp}>
+          <h2 style={{'fontWeight':'bold'}}>Cinem<span style={{'color':'#5ED5A8'}}>App</span></h2>
+          <Typography color='secondary' style={{fontSize:'30px'}}>Login</Typography>
+          <br/>
+          <p>Regístrate para ingresar a nuestra plataforma</p>
+              <form onSubmit={handleSubmit} className={classes.form} autoComplete='off' aria-autocomplete='off'>
+                <TextField
+                  defaultValue={``}
+                  name = 'email'
+                  type = 'text'
+                  fullWidth
+                  autoFocus
+                  size = 'small'
+                  color = 'primary'
+                  margin = 'normal'
+                  variant = 'outlined'
+                  label='Correo elecctrónico'
+                  value={input.email}
+                  onChange={handleChange}
+                  error={error?.email ? true:false}
+                />
+                <TextField
+                  defaultValue={``}
+                  name = 'password'
+                  type={`password`}
+                  value={input.password}
+                  fullWidth
+                  size='small'
+                  color='primary'
+                  margin='normal'
+                  variant='outlined'
+                  label='Contraseña'
+                  onChange={handleChange}
+                  error={error?.password?true:false}
+                />
+                <Typography component={`p`} style={{maxWidth: '350px', fontSize: '14px'}}>  
+                  El equipo de CinemApp bajo ninguna circunstancia pedira su correo o contraseña. 
+                </Typography>
+                <FormGroup>
+                  <FormControlLabel 
+                    control={<Checkbox 
+                      id='checkBox'
+                      name='checkBox'
+                      color='primary'
+                      value={check}
+                      onChange={handleChangeCheck}
+                    />} 
+                    label="Comprendo"
+                  />
+                </FormGroup>
+                <Button type='submit' variant='contained' color='primary' disableElevation
+                  disabled={(error?.email || error?.password || !check) ? true: false}
+                >Ingresar</Button>
+                <div style={{marginBottom:'7px'}}>
+                  <Typography component='span'>¿No tienes cuenta?
+                    <Link to='/register' style={{ color: '#0066ff', textDecoration: 'none',
+                      fontWeight: 'bold', marginLeft: '10px', marginRight:'10px', size:'14px' 
+                    }} >
+                      Regístrate
+                    </Link> o inicia con
+                  </Typography>
+                </div>
+              </form>
             <GoogleLogin
-            clientId="533216406102-cnhnnd2b69dvbkt69reehsd2e7stn4t4.apps.googleusercontent.com"
-            buttonText="Login with Google"
-            onSuccess={onGoogleSucces}
-            onFailure={onFailGoogle}
-            cookiePolicy={"single_host_origin"}
-          />
-  
-           
+              clientId="533216406102-cnhnnd2b69dvbkt69reehsd2e7stn4t4.apps.googleusercontent.com"
+              buttonText="Login with Google"
+              onSuccess={onGoogleSucces}
+              onFailure={onFailGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
         </div>
       </div>
     </div>
