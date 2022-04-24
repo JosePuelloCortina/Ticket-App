b35753cf89e880fe69903f2bbd45c84459deba8b
polacomark <you@example.com>
2022-04-24 17:20:43 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -26,6 +26,6 @@ const { conn } = require('./src/db.js');
 
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log("%s listening at 3000"); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
