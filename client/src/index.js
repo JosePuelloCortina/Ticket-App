25487fab050b45aec9aed2fd1600c426f9cd116d
polacomark <you@example.com>
2022-04-25 12:08:05 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
-//import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import store from "./redux/store/index";
 import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
-const herokuUrl = 'https://ticket-app-cine.herokuapp.com';
 
-axios.defaults.baseURL = process.env.REACT_APP_API || herokuUrl;
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <React.StrictMode>
